b678898e80c3726b2dc5bc1489c2ab8eec6172b9
johnshenk <jshenk@demandanlytics.io>
2023-02-03 12:22:30 -0600

    check auth on load

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import List from './components/List';
 import Edit from './components/Edit';
 import {ErrorContext, UserContext} from './Context';
 import './App.css';
+import { checkAuth } from './Api';
 
 const Error = ({ err }) => {
   return <div className='error'>{err}</div>;
@@ -14,6 +15,20 @@ const Error = ({ err }) => {
 function App() {
   const [user, setUser] = React.useState(JSON.parse(localStorage.getItem('user')));
   const [err, setErr] = React.useState();
+  
+  React.useEffect(() => {
+    if (!user || !checkAuth) {
+      return;
+    }
+    checkAuth({ token: user.credential })
+      .then((resp) => {
+        if (resp.error) {
+          localStorage.removeItem('user');
+          setUser(null);
+        }
+      })
+      .catch(setErr);
+  }, [user])
 
   const routes = useRoutes([
     { path: '/play/:id', element: <Play /> },
